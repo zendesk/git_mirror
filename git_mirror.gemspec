233d99c8b3d9126199819e8243ed31369a893588
Mick Staugaard <mick@staugaard.com>
2013-07-09 19:13:23 -0700

    Initial

---
+++ git_mirror.gemspec
@@ -0,0 +1,21 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = 'git_mirror'
+  spec.version       = '0.1'
+  spec.authors       = 'Mick Staugaard'
+  spec.email         = 'mick@zendesk.com'
+  spec.description   = 'utility for mirroring github repositories'
+  spec.summary       = 'utility for mirroring github repositories'
+  spec.homepage      = ''
+
+  spec.files         = Dir.glob('{lib,test,bin}/**/*') + ['README.md', 'bin/git-mirror']
+  spec.test_files    = Dir.glob('test/**/*')
+  spec.executables   = ['git-mirror']
+  spec.require_paths = ['lib']
+
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
+end
---
+++ lib/git_mirror.rb
@@ -0,0 +1,45 @@
+require 'pathname'
+
+class GitMirror
+  def initialize
+    @root = Pathname.new(ENV['GIT_MIRROR_ROOT'] || '/var/git-mirror')
+  end
+
+  def prepare_root
+    @root.mkpath
+  end
+
+  def update(repo_url)
+    mirror(repo_url) unless has_mirror?(repo_url)
+    update_local(local_path_for(repo_url))
+  end
+
+  def mirror(repo_url)
+    return if has_mirror?(repo_url)
+    execute "git clone --mirror --quiet #{repo_url} #{local_path_for(repo_url)}"
+  end
+
+  def has_mirror?(repo_url)
+    local_path_for(repo_url).directory?
+  end
+
+  def local_path_for(repo_url)
+    @root + repo_url.split(/github.com./, 2).last
+  end
+
+  def update_all
+    Pathname.glob(@root + '**/HEAD').each do |head|
+      update_local(head.parent)
+    end
+  end
+
+  def update_local(path)
+    execute "git fetch --prune --quiet #{path}"
+  end
+
+  def execute(command)
+    prepare_root
+    puts command
+    system(command)
+  end
+end
